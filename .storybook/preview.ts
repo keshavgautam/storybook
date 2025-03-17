7f4265565b77d674b44875ea40b3596f1637d1c0
keshav gautam <keshavgautam@keshavs-MacBook-Air.local>
2025-03-17 17:45:27 +0530

    update for  FAS-214

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,7 +3,8 @@ import type { Preview } from '@storybook/react'
 import 'tailwindcss/tailwind.css';
 import '../src/styles/globals.css';
 import '../src/styles/globals.scss';
-import '../src/styles/scss/main.scss';
+import '../public/scss/main.scss';
+
 const preview: Preview = {
   parameters: {
     controls: {
