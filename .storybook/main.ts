6c543ace41d8b70847304e5e20868ec1012c1a1c
keshav gautam <keshavgautam@keshavs-MacBook-Air.local>
2025-03-18 12:33:47 +0530

    update for  FAS-214

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,32 @@
 import type { StorybookConfig } from "@storybook/experimental-nextjs-vite";
+import { join, resolve,dirname } from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+    "@storybook/experimental-addon-test",
   ],
-  "framework": {
-    "name": "@storybook/experimental-nextjs-vite",
-    "options": {}
+  framework: {
+    name: "@storybook/experimental-nextjs-vite",
+    options: {},
   },
-  "staticDirs": [
-    "../public"
-  ]
+  viteFinal: async (config, { configType }) => {
+    // Add alias support for `@` to point to src/
+    config.resolve = {
+      alias: {
+        ...(config.resolve?.alias || {}),
+        "@": resolve(__dirname, "../src"),
+      },
+    };
+    return config;
+  },
+  staticDirs: ["../public"],
 };
 export default config;
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,9 +1,11 @@
 import type { Preview } from '@storybook/react'
+import React from 'react';
 
 import 'tailwindcss/tailwind.css';
 import '../src/styles/globals.css';
 import '../src/styles/globals.scss';
 import '../public/scss/main.scss';
+import Context from "../src/context/Context"
 const preview: Preview = {
   parameters: {
     controls: {
@@ -12,7 +14,15 @@ const preview: Preview = {
        date: /Date$/i,
       },
     },
+    
   },
+  decorators: [
+    (Story) => {
+
+      return (  <Context> <Story /></Context>)
+    },
+  ],
+  
 };
 
 export default preview;
